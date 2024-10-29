66a230f282a3ca8f7f1d3168098f5e0b60e67dbc
Zhaeong <owen_zzz@hotmail.com>
2024-10-29 03:01:13 -0400

    finished mlps

--- makemore/makemore_mlp.py
+++ makemore/makemore_mlp.py
@@ -3,6 +3,7 @@ import torch.nn.functional as torchF
 
 import matplotlib.pyplot as plt
 
+import random
 words = open('names.txt').read().splitlines()
 
 print(len(words))
@@ -24,51 +25,101 @@ for i in string_to_index:
 
 print(index_to_string)  
 
-# for training, this is the expected next char from the previous characters
-# we want to create neural network that takes the inputs and predicts the labels
-inputs = []
-labels = []
+## for training, this is the expected next char from the previous characters
+## we want to create neural network that takes the inputs and predicts the labels
+#inputs = []
+#labels = []
+#
+## context length: how many char do we need to predict the next one
+#block_size = 3
+#
+#for w in words:
+#    #print(w)
+#
+#    # 0 is the index for beginning or end char '.'
+#    # so start context with list of dots
+#    context = [0] * block_size
+#    for char in w + '.':
+#        char_index = string_to_index[char]
+#        inputs.append(context)
+#        labels.append(char_index)
+#
+#        inputString = ''
+#        for i in context:
+#            inputString += index_to_string[i]
+#        #print("input:", inputString, context, "Prediction:", index_to_string[char_index], char_index)
+#
+#        # increment to next char, crop list and append next
+#        context = context[1:] + [char_index]
+#
+#        # e.g.
+#        # input: .em [0, 5, 13] Prediction: m [13]
+#
+#
+#inputs = torch.tensor(inputs)
+#labels = torch.tensor(labels)
 
-# context length: how many char do we need to predict the next one
-block_size = 3
+def build_dataset(words):
+    # for training, this is the expected next char from the previous characters
+    # we want to create neural network that takes the inputs and predicts the labels
+    inputs = []
+    labels = []
+    
+    # context length: how many char do we need to predict the next one
+    block_size = 3
+    
+    for w in words:
+        #print(w)
+    
+        # 0 is the index for beginning or end char '.'
+        # so start context with list of dots
+        context = [0] * block_size
+        for char in w + '.':
+            char_index = string_to_index[char]
+            inputs.append(context)
+            labels.append(char_index)
+    
+            inputString = ''
+            for i in context:
+                inputString += index_to_string[i]
+            #print("input:", inputString, context, "Prediction:", index_to_string[char_index], char_index)
+    
+            # increment to next char, crop list and append next
+            context = context[1:] + [char_index]
+    
+            # e.g.
+            # input: .em [0, 5, 13] Prediction: m [13]
+    
+    
+    inputs = torch.tensor(inputs)
+    labels = torch.tensor(labels)
+    return inputs, labels
+    
 
-for w in words:
-    #print(w)
+random.seed(42)
+random.shuffle(words)
+percent80_words = int(0.8*len(words))
+percent90_words = int(0.9*len(words))
 
-    # 0 is the index for beginning or end char '.'
-    # so start context with list of dots
-    context = [0] * block_size
-    for char in w + '.':
-        char_index = string_to_index[char]
-        inputs.append(context)
-        labels.append(char_index)
-
-        inputString = ''
-        for i in context:
-            inputString += index_to_string[i]
-        #print("input:", inputString, context, "Prediction:", index_to_string[char_index], char_index)
-
-        # increment to next char, crop list and append next
-        context = context[1:] + [char_index]
-
-        # e.g.
-        # input: .em [0, 5, 13] Prediction: m [13]
+train_inputs, train_labels = build_dataset(words[:percent80_words])
+eval_inputs, eval_labels   = build_dataset(words[percent80_words:percent90_words])
+test_inputs, test_labels   = build_dataset(words[percent90_words:])
 
 
-inputs = torch.tensor(inputs)
-labels = torch.tensor(labels)
 
 # a mapping of character to 2D vectors
 CharLookupTable = torch.randn((27, 2))
 def training():
 
-    # forward pass
-    neurons = 100
+    # ==============================================
+
+    # making more parameters will take more steps for training
+    neurons = 300
 
     # hidden layer
     # 6 inputs because 2 dimensional embedding * 3 context length
     weights_1 = torch.randn((6, neurons))
-    biases_1 = torch.randn(100)
+    biases_1 = torch.randn(neurons)
 
     # now create output layer
     weights_2 = torch.randn(neurons, 27)
@@ -79,46 +130,85 @@ def training():
     for p in parameters:
         p.requires_grad = True
 
-    for i in range(1000):
+    # creates 1000 values between -3 and 0, steps linearly
+    learning_rate_exponent = torch.linspace(-3, 0, 1000)
+
+    # so 10^-3 to 10^0
+    # so 0.001 to 1
+    learning_rate_search = 10**learning_rate_exponent
+
+    learning_rate_used = []
+    loss_from_learning_rate = []
+    steps = []
+
+    final_loss = 100
+    for i in range(30000):
 
         # minibatch, so sample just a random number of input -> labels
         examples_used = 32
-        ix = torch.randint(0, inputs.shape[0], (examples_used,))
+        ix = torch.randint(0, train_inputs.shape[0], (examples_used,))
 
-        embedding = CharLookupTable[inputs[ix]]
+        # ==============================================
+        # forward pass
+        embedding = CharLookupTable[train_inputs[ix]]
         emb_cat = embedding.view(-1, 6)
         hidden_states = torch.tanh(emb_cat @ weights_1 + biases_1)
         logits = hidden_states @ weights_2 + biases_2
     
-        loss = torchF.cross_entropy(logits, labels[ix])
+        loss = torchF.cross_entropy(logits, train_labels[ix])
     
-        print(loss.item())
+        #print(loss.item())
     
+        # ==============================================
         # back propagation
         for p in parameters:
             p.grad = None
         loss.backward()
+        final_loss = loss.item()
+        
     
+        # ==============================================
         # update
+        # learning_rate = learning_rate_search[i]
+
+        # from graph 0.1 seems to be a good learning rate
+        # you can decay learning rate to 0.01 at the end to get better loss
         learning_rate = 0.1
         for p in parameters:
             p.data += -learning_rate * p.grad
+
+        # track stats
+
+        # exponent of the learning rate
+        # learning_rate_used.append(learning_rate_exponent[i])
+
+        # plotting log loss so the curve is smoother
+        loss_from_learning_rate.append(loss.log10().item())
+        steps.append(i)
+
+    print("train dataset loss:", final_loss)
+    # look for the valley to pick good learning rate
+    #plt.plot(learning_rate_used, loss_from_learning_rate)
+    plt.plot(steps, loss_from_learning_rate)
+    plt.show()
     
     return parameters
 
+# evaluating on eval dataset
 def evaluate_loss(parameters):
 
     # indices based on parameters = [CharLookupTable, weights_1, biases_1, weights_2, biases_2]
-    embedding = parameters[0][inputs]
+    embedding = parameters[0][eval_inputs]
     emb_cat = embedding.view(-1, 6)
     hidden_states = torch.tanh(emb_cat @ parameters[1] + parameters[2])
     logits = hidden_states @ parameters[3] + parameters[4]
     
-    loss = torchF.cross_entropy(logits, labels)
+    loss = torchF.cross_entropy(logits, eval_labels)
     
-    print("Full dataset loss:", loss.item())
+    print("eval dataset loss:", loss.item())
 
 def generate_samples(parameters, num_samples):
+    block_size = 3
     for i in range(num_samples):
         output = ''
         context = [0] * block_size
@@ -136,6 +226,14 @@ def generate_samples(parameters, num_samples):
                 break
         print(output)
 
+# can only graph with 2d embeddings
+def graph_char_embeddings():
+    plt.figure(figsize=(8,8))
+    plt.scatter(CharLookupTable[:,0].data, CharLookupTable[:,1].data, s=200)
+    for i in range(CharLookupTable.shape[0]):
+        plt.text(CharLookupTable[i,0].item(), CharLookupTable[i,1].item(), index_to_string[i], ha="center", va="center", color='white')
+    plt.grid('minor')
+    plt.show()
 
 if __name__ == "__main__":
     print("Start")
@@ -143,6 +241,7 @@ if __name__ == "__main__":
     params = training()
 
     evaluate_loss(params)
+    graph_char_embeddings()
 
     generate_samples(params, 20)
     exit()
